f569c0a571daaa1b39f82bd9d8dabd2c620aa9cc
chidi ezek <chidi.ezekwonna@gmail.com>
2022-09-22 11:30:19 +0200

    first commit

---
+++ src/main/java/com/example/springrestchildcare/SpringRestChildcareApplication.java
@@ -0,0 +1,13 @@
+package com.example.springrestchildcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestChildcareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestChildcareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springrestchildcare/SpringRestChildcareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springrestchildcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestChildcareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
